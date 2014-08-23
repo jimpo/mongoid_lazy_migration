98fa59be6454306d5f99db2ca8335ea43100037a
Jim Posen <jim@coinbase.com>
2014-08-23 14:44:51 -0700

    Removed code made obsolete from prior changes.

--- lib/mongoid/lazy_migration/tasks.rb
+++ lib/mongoid/lazy_migration/tasks.rb
@@ -30,13 +30,6 @@ module Mongoid::LazyMigration::Tasks
         "Remove the migration from your model before cleaning up the database")
     end
 
-    if model.where(:migration_state => :processing).limit(1).count > 0
-      raise ["Some models are still being processed.",
-             "Remove the migration code, and go inspect them with:",
-             "#{model}.where(:migration_state => :processing))",
-             "Don't forget to remove the migration block"].join("\n")
-    end
-
     selector = { :migration_state => { "$exists" => true }}
     changes  = {"$unset" => { :migration_state => 1}}
     safety   = { :safe => true, :multi => true }
